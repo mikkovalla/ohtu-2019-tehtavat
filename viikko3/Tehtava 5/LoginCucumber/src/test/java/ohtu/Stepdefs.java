f05b5c9b1b258d9458c8e7617f96c0883e0499fa
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-11-18 21:27:56 +0200

    stepdefs updated

--- viikko3/Tehtava 5/LoginCucumber/src/test/java/ohtu/Stepdefs.java
+++ viikko3/Tehtava 5/LoginCucumber/src/test/java/ohtu/Stepdefs.java
@@ -43,6 +43,18 @@ public class Stepdefs {
     @Then("system will respond with {string}")
     public void systemWillRespondWith(String expectedOutput) {
         assertTrue(io.getPrints().contains(expectedOutput));
-    }    
+    }   
+    
+    @Given("^command new is selected$")
+    public void commandNewSelected() throws Throwable {
+        inputLines.add("new");
+    }
+
+    @Given("user {string} with password {string} is created")
+    public void usernameAndPasswordIsCreated(String username, String password) {
+       inputLines.add("new");
+       inputLines.add(username);
+       inputLines.add(password);
+    } 
 
 }
