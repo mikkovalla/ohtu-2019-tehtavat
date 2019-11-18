726500a19af695709596235c50b8d2105cf543cd
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-11-18 22:50:41 +0200

    ex 8 done

--- viikko3/Tehtava 7/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/Tehtava 7/WebLogin/src/main/java/ohtu/Tester.java
@@ -8,27 +8,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Miksu\\Downloads\\chromedriver_win32\\chromedriver.exe"); 
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
         
         sleep(2);
-        
         WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep12");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+        
+        sleep(2);
+
+        element = driver.findElement(By.linkText("register new user"));
         element.click();
 
         sleep(2);
 
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("mikko");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("mikko1234");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("mikko1234");
+        element = driver.findElement(By.name("signup"));
         
         sleep(2);
         element.submit();
 
         sleep(3);
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+        
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        sleep(2);
         
         driver.quit();
     }
