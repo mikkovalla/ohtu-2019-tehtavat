47f95cdf3660c27c36702c5cf58d9596bb717aad
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-11-18 20:00:20 +0200

    added getters and setters to Player class

--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,18 +3,78 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private String team;
+    private int goals;
+    private int assists;
 
     public void setName(String name) {
         this.name = name;
     }
 
     public String getName() {
-        return name;
+        return this.name;
+    }
+
+    /**
+     * @param nationality the nationality to set
+     */
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    /**
+     * @param team the team to set
+     */
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    /**
+     * @param goals the goals to set
+     */
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    /**
+     * @param assists the assists to set
+     */
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    /**
+     * @return the nationality
+     */
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    /**
+     * @return the team
+     */
+    public String getTeam() {
+        return this.team;
+    }
+
+    /**
+     * @return the goals
+     */
+    public int getGoals() {
+        return this.goals;
+    }
+
+    /**
+     * @return the assists
+     */
+    public int getAssists() {
+        return this.assists;
     }
 
     @Override
     public String toString() {
-        return name;
+        return this.name;
     }
       
 }
