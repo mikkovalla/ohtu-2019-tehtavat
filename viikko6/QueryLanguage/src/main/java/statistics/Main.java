fb0d88cd0541ad259c8dc90be5f44d211ac3f862
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-12-09 22:03:12 +0200

    ex3 done

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -11,10 +11,14 @@ public class Main {
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
 
-        Matcher m = new And( 
-            new Not( new HasAtLeast(1, "goals") ), 
-            new PlaysIn("NYR")
-        );
+        Matcher m = new And(
+            new HasAtLeast(20, "points"),
+            new Or( 
+                new PlaysIn("NYR"),
+                new PlaysIn("NYI"),
+                new PlaysIn("NJD")
+            )
+        ); 
         
         for (Player player : stats.matches(m)) {
             System.out.println(player);
